46aba4ac30bbc6bdb5f032de4f7152a6ed32a46f
dearm762 <misterfighter1990@gmail.com>
2024-01-17 22:54:43 +0600

    third commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About page',
+  description: 'Its about page description'
+}
+
 const About = () => {
   return (
     <h1>Welcome About!!!</h1>
---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+import { Metadata } from 'next'
+
+export const generateMetadata = ({ params }:  UserProps ): Metadata => {
+	return {
+		title: `User ${ params.userId }`
+	}
+}
+
+type UserProps = {
+	params: {
+		userId: string
+	}
+}
+
+const User: FC<UserProps> = ({ params }) => {
+	return (
+		<>
+			<h1>User Id: { params.userId }</h1>
+		</>
+	)
+}
+
+export default User
