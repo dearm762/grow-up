3b0537c2cf977963a5a61d628f4ce704f1146c65
dearm762 <misterfighter1990@gmail.com>
2024-01-19 11:49:26 +0600

    8 commit

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const routes = [
 const Home: FC = () => {
   return (
     <>
-      <h1 className={ styles.heading }>Welcome Home!!!</h1>
+      <h1 className={ styles.heading }>Welcome to Home page!!!</h1>
       <h2 className={ styles.heading2 }>SiteMap:</h2>
       <nav className={ styles.navigation }>
         {
