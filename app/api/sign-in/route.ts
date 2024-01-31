f7eed0176f6604008146a365532740816cade97c
dearm762 <misterfighter1990@gmail.com>
2024-01-31 08:50:00 +0600

    changes in post req

--- app/api/sign-in/route.ts
+++ app/api/sign-in/route.ts
@@ -1,11 +1,7 @@
 export const POST = async (request: Request) => {
 	const data = await request.json()
-	const res = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ status: 'success' }),
-  }
- 
-  return Response.json(res)
+	
+  return Response.json({
+		status: 'success'
+	})
 }
