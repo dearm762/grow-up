f282b694281554589824672bf47b19002ee36177
dearm762 <misterfighter1990@gmail.com>
2024-02-01 20:43:05 +0600

    changes in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <>
       <h1>Welcome to about page!!!</h1>
-      <p>In honestly this is my first next js website</p>
+      <p>This page still in developing</p>
     </>
   )
 }
