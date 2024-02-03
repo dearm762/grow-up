bf8010975d48e6d22d67dc98e5fe655d1b4f4fe8
dearm762 <misterfighter1990@gmail.com>
2024-02-03 15:53:38 +0600

    changes with inputs

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,13 +1,51 @@
+'use client'
+
 import FormHeading from '@/app/_system/components/FormHeading'
 import { Input } from '@nextui-org/react'
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { EyeFilledIcon } from '@/app/_system/EyeFilledIcon'
+import { EyeSlashFilledIcon } from '@/app/_system/EyeSlashFilledIcon'
 
 const SignIn: FC = () => {
+	const [isVisible, setIsVisible] = useState(false)
+
+	const toggleVisibility = () => setIsVisible(!isVisible)
+
 	return (
 		<>
 			<FormHeading>Sign In</FormHeading>
-			<Input label='Email' />
-			<Input label='Password' />
+			<Input
+				label='Email'
+				variant='underlined'
+				color='primary'
+				size='sm'
+				isRequired
+				radius='none'
+				isClearable
+				onClear={() => console.log('input cleared')}
+			/>
+			<Input
+				label='Password'
+				variant='underlined'
+				color='primary'
+				size='sm'
+				isRequired
+				radius='none'
+				endContent={
+					<button
+						className='focus:outline-none'
+						type='button'
+						onClick={toggleVisibility}
+					>
+						{isVisible ? (
+							<EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+						) : (
+							<EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+						)}
+					</button>
+				}
+				type={isVisible ? 'text' : 'password'}
+			/>
 			<button>Sign In</button>
 		</>
 	)
