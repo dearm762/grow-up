5e6c11abd10d156538cf6d90e961f20f65e442f9
dearm762 <misterfighter1990@gmail.com>
2024-02-08 18:15:44 +0600

    routes fixes

--- app/_system/routes.ts
+++ app/_system/routes.ts
@@ -1,18 +1,18 @@
 type routeType = {
-  name: string,
-  path: string
+	name: string,
+	path: string
 }
 
 const routes: Array<routeType> = [
-  { name: 'Home', path: '/' },
-  { name: 'About', path: '/about' },
-  { name: 'Forgot Password', path: '/auth/forgot-password' },
-  { name: 'Sign In', path: '/auth/sign-in' },
-  { name: 'Sign Up', path: '/auth/sign-up' },
-  { name: 'Users/[userId]', path: '/users/1' },
-  { name: 'Photos', path: '/photos' },
-  { name: 'Photos/[photoId]', path: '/photos/1' },
-  { name: 'Get photos via API', path: '/api/get-photos' }
+	{ name: 'Home', path: '/' },
+	{ name: 'About', path: '/about' },
+	{ name: 'Forgot Password', path: '/auth/forgot-password' },
+	{ name: 'Sign In', path: '/auth/sign-in' },
+	{ name: 'Sign Up', path: '/auth/sign-up' },
+	{ name: 'Users/[userId]', path: '/users/1' },
+	{ name: 'Photos', path: '/photos' },
+	{ name: 'Photos/[photoId]', path: '/photos/1' },
+	{ name: 'Get photos via API', path: '/api/get-photos' },
 ]
 
 export default routes
