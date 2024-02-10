84830c01e8035413fc35f483434a76d1f2494bc5
dearm762 <misterfighter1990@gmail.com>
2024-02-10 17:26:25 +0600

    modifying home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 import { FC } from 'react'
-import routes from '@/data/routesData'
+import { ScanEye } from 'lucide-react'
 import styles from './home.module.css'
 
 const Home: FC = () => {
 	return (
 		<>
-			<h1 className={styles.heading}>Welcome to Home page!!!</h1>
-			<h2 className={styles.heading2}>Current available Routes:</h2>
-			<nav className={styles.navigation}>
-				{routes.map(route => (
-					<Link key={route.path} href={route.path}>
-						{route.name}
-					</Link>
-				))}
-			</nav>
+			<header className={styles.header}>
+				<div>
+					<ScanEye width={45} height={45} />
+					<span>PrivateWorld</span>
+				</div>
+				<nav>
+					<Link href={'/'}>Home</Link>
+					<Link href={'/blog'}>Blog</Link>
+					<Link href={'/Sign-in'}>Sign In</Link>
+				</nav>
+			</header>
 		</>
 	)
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,18 +1,9 @@
-'use client'
-
 import { FC } from 'react'
 
-interface ITest {
-	searchParams: {
-		message: string
-		b?: string
-	}
-}
-
-const Test: FC<ITest> = ({ searchParams }) => {
+const Test: FC = () => {
 	return (
 		<>
-			<h2>{searchParams.message + ' ' + searchParams.b}</h2>
+			<button>Click</button>
 		</>
 	)
 }
