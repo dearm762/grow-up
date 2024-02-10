00e77d8e833d66ef398b86313801dcc3e63bc5d7
dearm762 <misterfighter1990@gmail.com>
2024-02-10 16:21:50 +0600

    learning searchParams

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,18 @@
+'use client'
+
 import { FC } from 'react'
 
 interface ITest {
 	searchParams: {
-		a: string
-		b: string
+		message: string
+		b?: string
 	}
 }
 
 const Test: FC<ITest> = ({ searchParams }) => {
 	return (
 		<>
-			<h2>{searchParams.a}</h2>
+			<h2>{searchParams.message + ' ' + searchParams.b}</h2>
 		</>
 	)
 }
