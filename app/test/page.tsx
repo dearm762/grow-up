998590c93b1d86a50154ac0ade037c5d8e529bee
dearm762 <misterfighter1990@gmail.com>
2024-02-12 06:34:24 +0600

    playing with useRouter

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import { FC } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Test: FC = () => {
+	const router = useRouter()
 	return (
 		<>
-			<button>Click</button>
+			<button onClick={() => router.push('/about')}>Click</button>
+			<Link href='/'>sss</Link>
 		</>
 	)
 }
