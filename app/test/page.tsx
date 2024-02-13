1a833c75d6ca805f00329e9571a719b3b13cbfa6
dearm762 <misterfighter1990@gmail.com>
2024-02-13 17:16:37 +0600

    changes in test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { FC } from 'react'
+import Image from 'next/image'
 
 const Test: FC = () => {
 	return (
 		<>
+			<Image src={'/me.jpg'} alt='alt of photo' width={100} height={100} />
 		</>
 	)
 }
