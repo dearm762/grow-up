7796dc7ca3afe824c7dab075d051ff6b4537215b
dearm762 <misterfighter1990@gmail.com>
2024-02-14 20:26:54 +0600

    checking if useeffect works well

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import Image from 'next/image'
 
 const Test: FC = () => {
+	useEffect(() => console.log(123))
+
 	return (
 		<>
 			<Image src={'/me.jpg'} alt='alt of photo' width={100} height={100} />
