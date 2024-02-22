f6f2e6583a93b12e693e526a40de7f10b6909769
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-02-22 06:08:30 -0800

    added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { FC } from 'react'
 import Header from '@/components/header'
 import containerStyle from '@/styles/container.module.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'KeepInTouch | Welcome!',
+	description: 'First social media by Kazakhstan'
+}
 
 const Home: FC = () => {
 	return (
 		<>
 			<Header />
 			<main className={containerStyle.mycontainer}>
-				<h1>Main page of KeepInTouch</h1>
+				
 			</main>
 		</>
 	)
