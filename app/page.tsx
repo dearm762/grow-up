b09f661d8ec43554927b625f480d5fa5dfea1b83
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-02-22 06:20:54 -0800

    added keywords

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 import { FC } from 'react'
-import Header from '@/components/header'
-import containerStyle from '@/styles/container.module.css'
 import type { Metadata } from 'next'
+
+import Header from '@/components/header'
+
+import containerStyle from '@/styles/container.module.css'
 
 export const metadata: Metadata = {
 	title: 'KeepInTouch | Welcome!',
-	description: 'First social media by Kazakhstan'
+	description: 'First social media by Kazakhstan',
+	keywords: [
+		"Kazakhstan", "Astana", "Almaty",
+    	"Central Asia", "Silk Road", "Nomad",
+    	"Steppe", "Culture", "Tradition",
+    	"History", "Kazakh", "Russian",
+    	"Ethnicity", "Diversity", "Hospitality", 
+		"Nature", "Mountains", "Lakes",
+    	"Tourism", "Adventure", "Cuisine",
+    	"Baikonur", "Space", "Technology",
+    	"Economy", "Business", "Innovation",
+    	"Education", "Youth", "Fashion",
+    	"Art", "Music", "Film",
+    	"Literature", "Sports", "Football",
+    	"Wrestling", "Ice Skating", "National Holidays",
+    	"Independence", "Unity", "Multiculturalism",
+    	"Language", "Cyrillic", "Urban Life",
+    	"Rural Life", "Family", "Friendship",
+    	"Networking", "Social Change"
+	]
 }
 
 const Home: FC = () => {
@@ -13,7 +34,7 @@ const Home: FC = () => {
 		<>
 			<Header />
 			<main className={containerStyle.mycontainer}>
-				
+				<h1>New Era of Messaging</h1>
 			</main>
 		</>
 	)
