63a3bc6159a2a9f016486054d0298e9c9e157d3b
= <misterfighter1990@gmail.com>
2024-02-24 09:31:30 -0800

    fix userouter

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react'
+import type { Metadata } from 'next'
 
 import Header from '@/components/header'
 import Heading from '@/components/heading'
 
 import containerStyle from '@/styles/container.module.css'
+
+export const metadata: Metadata = {
+	title: 'KeepInTouch | Blog',
+}
 
 const BlogPage: FC = () => {
 	return (
--- components/header.tsx
+++ components/header.tsx
@@ -7,12 +7,12 @@ import styles from '@/styles/header.module.css'
 const Header: FC = () => {
 	return (
 		<header className={styles.header}>
-			<div>
+			<Link href={'/'}>
 				<ScanEye width={45} height={45} />
 				<h1>KeepInTouch</h1>
-			</div>
+			</Link>
 			<nav>
-				<Link href={'/'}>Home</Link>
+				<Link href={'/home'}>Home</Link>
 				<Link href={'/blog'}>Blog</Link>
 				<Link href={'/auth/sign-in'}>Sign In</Link>
 			</nav>
