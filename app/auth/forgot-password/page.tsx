69913b9e04773440ee602912bc3b955862032425
= <misterfighter1990@gmail.com>
2024-02-26 01:27:22 -0800

    forget password redisigned

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,7 +1,34 @@
-const ForgotPassword = () => {
-	return (
-	  <div>ForgotPassword</div>
-	)
-  }
-  
-  export default ForgotPassword
+'use client'
+
+import Link from 'next/link'
+import signIn from '@/actions/signIn'
+
+import styles from '@/styles/form.module.css'
+
+const ForgotPage = () => {
+  return (
+    <>
+      <title>KeepInTouch | Forgot Password</title>
+      <div className={styles.wrapper}>
+        <div className={ styles.formAuth }>
+          <form className={ styles.form } action={signIn} method='POST'>
+            <h1 className={ styles.heading }>Forgot Password</h1>
+            <input className={ styles.input } type="email" name="email" placeholder="Email" required />
+            <button type="submit" className={ styles.button }>Sign In</button>
+          </form>
+        </div>
+        <div className={ styles.overlayContainer }>
+          <div className={ styles.overlay }>
+            <div className={ styles.overlayPanelRight }>
+              <h1 className={ styles.heading }>Hello, Friend!</h1>
+              <p className={ styles.paragraph }>Enter your personal details and start journey with us</p>
+              <Link href='/auth/sign-in' className={ styles.ghost } >Sign In</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ForgotPage
