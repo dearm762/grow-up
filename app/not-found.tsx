16dfb8bbdec2284b96159184a43d853847a5f3fb
= <misterfighter1990@gmail.com>
2024-02-26 04:26:50 -0800

    error404 page designed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,26 @@
 import { FC } from 'react'
+import type { Metadata } from 'next'
+import Link from 'next/link'
+
+import styles from '@/styles/error404.module.css'
+
+export const metadata: Metadata = {
+	title: 'KeepInTouch | Page not found'
+}
 
 const Error404: FC = () => {
 	return (
-		<>
-			<h1>404</h1>
-			<p>defaut site&apos;s not found page</p>
-		</>
+		<div className={styles.flexContainer}>
+		  <div className={styles.textCenter}>
+			<h1>
+			  <span className={styles.fadeIn} id="digit1">4</span>
+			  <span className={styles.fadeIn} id="digit2">0</span>
+			  <span className={styles.fadeIn} id="digit3">4</span>
+			</h1>
+			<h3>PAGE NOT FOUND</h3>
+			<Link type="button" href={'/'}>Return To Home</Link>
+		  </div>
+		</div>
 	)
 }
 
