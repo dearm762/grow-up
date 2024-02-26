709007573a5f94df5cff2b2993c86c560a820d34
= <misterfighter1990@gmail.com>
2024-02-26 04:00:15 -0800

    code cleaned

--- app/api/get-photo/[id]/route.tsx
+++
@@ -1,9 +0,0 @@
-import photoData from '@/data/photoData'
-
-export const GET = async (_request: Request, { params }: { params: { id: string }}) => {
-	const photo = photoData.find(
-		photo => photo.id === params.id
-	)
-
-	return Response.json(photo)
-}
--- app/api/get-photos/route.ts
+++
@@ -1,3 +0,0 @@
-import photoData from '@/data/photoData'
-
-export const GET = async (_request: Request) => Response.json(photoData)
