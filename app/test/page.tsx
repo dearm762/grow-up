0cb0dab26a0f098ac04d6fc83bd79f79b898d60d
= <misterfighter1990@gmail.com>
2024-02-28 11:12:02 +0600

    create test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,13 @@
-const Test = () => {
-  return (
-	<div>Test</div>
-  )
+'use client'
+
+import { FC } from 'react'
+
+const TestPage: FC = () => {
+	return (
+		<>
+			<h1>Test Page</h1>
+		</>
+	)
 }
 
-export default Test
+export default TestPage
