994b68a23ccf5e5ef4d261e48cf7e3027722396c
dearm762 <misterfighter1990@gmail.com>
2024-03-04 13:14:15 +0600

    new active and passive buttons

--- components/courses.tsx
+++ components/courses.tsx
@@ -8,23 +8,23 @@ import styles from '@/styles/courses.module.css'
 type category = 'all' | 'alphabet' | 'new' | 'popular'
 
 const Courses: FC = () => {
-	const [category, setCategory] = useState<category>()
+	const [category, setCategory] = useState<category>('all')
 
 	return (
 		<>
 			<div
 				className={
-					containerStyle.mycontainer + ' px-9 bg-cyan-100 my-16 rounded-3xl'
+					containerStyle.mycontainer + ' px-9 bg-cyan-400 my-16 rounded-3xl'
 				}
 			>
 				<div className={styles.courses}>
 					<h2 className={styles.heading}>Измени жизнь этой весной</h2>
 					<div className={styles.categories}>
 						<h2>Категорий:</h2>
-						<button>Все курсы</button>
-						<button>алфавиту</button>
-						<button>Новое</button>
-						<button>Популярные</button>
+						<button className={ category == 'all' ? styles.activeButton : styles.button }>Все курсы</button>
+						<button className={ category == 'alphabet' ? styles.activeButton : styles.button }>алфавиту</button>
+						<button className={ category == 'new' ? styles.activeButton : styles.button }>Новое</button>
+						<button className={ category == 'popular' ? styles.activeButton : styles.button }>Популярные</button>
 					</div>
 				</div>
 			</div>
